838845bdd369f342b0af6a4f490c1d65585a717a
Renzuky <renzoxdgamer@gmail.com>
2026-06-23 22:58:31 -0500

    Ejecutable 2.0

--- run-app.bat
+++ run-app.bat
@@ -1,3 +1,11 @@
 @echo off
 echo "Iniciando el servidor de desarrollo de Laravel..."
-php artisan serve
+
+:: Inicia el servidor de Laravel en una nueva ventana de terminal.
+start "Laravel Dev Server" php artisan serve
+
+:: Espera 5 segundos para dar tiempo a que el servidor se inicie correctamente.
+timeout /t 5 /nobreak > nul
+
+:: Abre la URL de la aplicación en el navegador predeterminado.
+start http://127.0.0.1:8000
